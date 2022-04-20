cc2e4ec487eea8dedbedcd9f02e5498aad061bfc
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-04-20 18:18:17 +0200

    add index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,102 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body class="bg-stone-200">
+    <!--Registro usuario-->
+    <div
+        class="block p-6 rounded-lg shadow-lg bg-white max-w-sm mx-auto mt-32 bg-red-100 "
+    >
+        <form action="/api/registro" method="post">
+            <div class="form-group mb-6">
+                <label
+                    for="nombre"
+                    class="form-label inline-block mb-2 text-gray-700"
+                    >Nombre</label
+                >
+                <!--input nombre-->
+                <input
+                    type="text"
+                    class="form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="name"
+                    name="name"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter name"
+                    required
+                />
+            </div>
+            <div class="form-group mb-6">
+                <label
+                    for="exampleInputPassword2"
+                    class="form-label inline-block mb-2 text-gray-700"
+                    >Contraseña</label
+                >
+                <!--input contraseña-->
+                <input
+                    type="password"
+                    class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    id="exampleInputPassword2"
+                    placeholder="Password"
+                    required
+                />
+            </div>
+            <div class="flex justify-between items-center mb-6">
+                <div class="form-group form-check">
+                    <input
+                        type="checkbox"
+                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                        id="exampleCheck2"
+                    />
+                    <label
+                        class="form-check-label inline-block text-gray-800"
+                        for="exampleCheck2">Recordarme</label
+                    >
+                </div>
+            </div>
+            <button
+                type="submit"
+                class="
+        w-full
+        px-6
+        py-2.5
+        bg-blue-600
+        text-white
+        font-medium
+        text-xs
+        leading-tight
+        uppercase
+        rounded
+        shadow-md
+        hover:bg-blue-700 hover:shadow-lg
+        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg
+        transition
+        duration-150
+        ease-in-out">ingresar</button
+            >
+        </form>
+    </div>
+</body>
