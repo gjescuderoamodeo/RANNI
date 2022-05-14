ec9581ea0190c5e9bd5164fa8ff23519ea73f6ff
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-05-14 14:18:02 +0200

    Add change status pedido
    Cuando se cambia el estado del pedido por parte del cocinero, se restan las cantidades correspondientes al inventario de ingredientes

--- src/routes/api/changeStatusPlatoPedido.js
+++ src/routes/api/changeStatusPlatoPedido.js
@@ -17,13 +17,17 @@ export async function put({ request }) {
             }
         })
 
-        if (getPlatePut) {
-            const putPlate = await prisma.plato_Pedido.update({
+        if (getPlatePut) {            
+
+            removeIngredientsFromAlmacen(getPlatePut.id,body.id_pedido);
+
+            const putPlate = await prisma.plato_Pedido.updateMany({
                 data: {
                     estado: 'Confirmado',
+                    //estado: 'En_Proceso',
                 },
                 where: {
-                    id_plato: body.id_plato,
+                    plato_id: body.id_plato,
                 },
             });
 
@@ -46,5 +50,64 @@ export async function put({ request }) {
         }
     }
 
+}
+
+//función para quitar al almacen la cantidad de los ingredientes
+async function removeIngredientsFromAlmacen(plato_id, pedido_id) {
+    try{
+        //lista con todos los ingredientes del plato (solo quiero sus id)
+        platoIngredientes = await prisma.plato_Ingrediente.findMany({
+            select:{
+                cantidad: true,
+                ingrediente_id: true,
+                plato_id: true
+            },
+            where: {
+                plato_id: plato_id
+            }
+        })  
+
+
+        //una vez sacado la lista, recorro dicha lista y resto las cantidades a los ingredientes
+        for (let i = 0; i < platoIngredientes.length; i++) { 
+            let cantidadI = await prisma.ingrediente.findFirst({
+                select:{
+                    cantidad: true,
+                },
+                where:{
+                    id: platoIngredientes[i].ingrediente_id
+                }
+            })
+
+            //cantiad de dicho plato en el pedido
+            let cantidadP = await prisma.plato_Pedido.findFirst({
+                select:{
+                    cantidad: true,
+                },
+                where:{
+                    pedido_id: pedido_id,
+                    plato_id: platoIngredientes[i].plato_id
+                }
+            })
+
+            let actualizarCantidadIngrediente = await prisma.ingrediente.updateMany({
+                data: {
+                    cantidad: (cantidadI.cantidad-(platoIngredientes[i].cantidad*cantidadP.cantidad)),
+                },
+                where: {
+                    id: platoIngredientes[i].ingrediente_id,
+                },
+            });
+        }
+
+        
+
+    }catch (error) {
+        console.log(error);
+        console.log("Error al remover los ingredientes");
+        return {
+            status: 400
+        }
+    }
 
 }
