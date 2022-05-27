faebbad23342574d224330245babf271b4317c24
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-05-27 12:40:46 +0200

    fix bug prisma web

--- src/routes/api/ingredientesPlato.js
+++ src/routes/api/ingredientesPlato.js
@@ -1,8 +1,8 @@
-import prismaImport from '@prisma/client'
-
-const { PrismaClient } = prismaImport;
-
+import Prisma, * as PrismaScope from "@prisma/client";
+const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 const prisma = new PrismaClient();
+export default prisma;
+
 
 export async function get() {
     let ingredientePatos = await prisma.plato_Ingrediente.findMany({
