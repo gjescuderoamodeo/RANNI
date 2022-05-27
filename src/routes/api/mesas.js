97fdc2efe5c82f5fdf83b1226bb7f6b2d0b985d6
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-05-27 12:42:44 +0200

    fix bug prisma web

--- src/routes/api/mesas.js
+++ src/routes/api/mesas.js
@@ -1,8 +1,8 @@
-import prismaImport from '@prisma/client'
-
-const { PrismaClient } = prismaImport;
-
+import Prisma, * as PrismaScope from "@prisma/client";
+const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 const prisma = new PrismaClient();
+export default prisma;
+
 
 export async function get() {
     let mesas = await prisma.mesa.findMany();
