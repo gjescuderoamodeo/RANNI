dec4dec33574aba20e7c38978f58779f58b9bb4e
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2022-05-27 12:45:04 +0200

    fix bug prisma web

--- src/routes/api/totalGanancias.js
+++ src/routes/api/totalGanancias.js
@@ -1,7 +1,8 @@
-import prismaImport from '@prisma/client'
-const { PrismaClient } = prismaImport;
-
+import Prisma, * as PrismaScope from "@prisma/client";
+const PrismaClient = Prisma?.PrismaClient || PrismaScope?.PrismaClient;
 const prisma = new PrismaClient();
+export default prisma;
+
 
 //función que da las ganancias totales en las facturas
 export async function get() {
